a3c4f04b1756f9d130c55f3e96ee37728fb1e92c
shogo <shogo729@gmail.com>
2020-06-16 01:06:07 +0900

    make template

---
+++ answer.cpp
@@ -0,0 +1,10 @@
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int MOD = 1000000007;
+
+int main() {
+    return 0;
+}
