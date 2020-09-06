4d1b7ad7f9455cfbc64fa8b31e0c25c1fbbea0cf
shogo <shogo729@gmail.com>
2020-09-06 23:55:52 +0900

    ABC155C-ABC156C

---
+++ ABC/156/C/answer.cpp
@@ -0,0 +1,31 @@
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int MOD = 1000000007;
+// MAX int 2,147,483,647 -> 10桁
+// MAX ll 9,223,372,036,854,775,807 -> 19桁
+// MAX O(n) 10^18 -> 18回
+// double pi = M_PI or acos(-1)
+// printf("%.10f\n", ans);
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> x(n);
+    rep(i, n) cin >> x.at(i);
+
+    int ans = MOD;
+    rep(p, 300) {
+        int sum = 0;
+        rep(i, n) {
+            sum += (x.at(i) - p) * (x.at(i) - p);
+        }
+        ans = min(ans, sum);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
